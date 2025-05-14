42652c4ba18926e16fb960fd773542c6389fb79d
François Fornaciari <francois.fornaciari@gmail.com>
2025-05-14 22:03:30 +0200

    update

--- blocks/3d-viewer/3d-viewer.js
+++ blocks/3d-viewer/3d-viewer.js
@@ -5,6 +5,7 @@ export default function decorate(block) {
   console.info(config.asset)
 
   const content = document.createRange().createContextualFragment(`
+    <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/4.0.0/model-viewer.min.js"></script>
     <model-viewer style="width: 100%; height: 500px;"
       src="${config.asset}"
       ar shadow-intensity="1" camera-controls touch-action="pan-y">
