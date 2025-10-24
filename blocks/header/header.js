03bd2df76623252f3e58a69bf5b50a83c2f5f54c
François Fornaciari <francois.fornaciari@gmail.com>
2025-10-24 07:18:19 +0200

    update

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -1,8 +1,12 @@
 
 export default async function decorate(block) {
+
+  const content = document.createRange().createContextualFragment(`
+    <div>Header Component</div>
+  `);
   
   block.textContent = '';
-  block.append("<div>Header Component</div>");
+  block.append(content);
 
   
 }
