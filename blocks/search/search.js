d8ecd4d344eab48619c05652e37e7415c2b83288
François Fornaciari <francois.fornaciari@gmail.com>
2025-11-15 16:33:38 +0100

    update

--- blocks/search/search.js
+++ blocks/search/search.js
@@ -15,6 +15,10 @@ export default async function decorate(block) {
 
       <form class="search-form" role="search">
         <div class="search-wrapper">
+          <svg class="search-icon" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
+            <circle cx="11" cy="11" r="8"/>
+            <path d="m21 21-4.35-4.35"/>
+          </svg>
           <input
             type="search"
             class="search-input"
