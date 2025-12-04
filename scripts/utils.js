f966f34561d0f99cb294e5c6377f47ead6444297
bamy <arthur.bamy@ik.me>
2025-12-04 16:06:40 +0100

    update hero and offer endpoint

--- scripts/utils.js
+++ scripts/utils.js
@@ -112,7 +112,7 @@ export async function fetchContentFragmentByPath(fragmentPath) {
   try {
     const apiUrl = isAuthorMode
       ? `${AUTHOR_DOMAIN}/adobe/sites/cf/fragments?path=${fragmentPath}&references=direct`
-      : `${PUBLISH_DOMAIN}/graphql/execute.json/gs/offerByPath;offerPath=${fragmentPath}`;
+      : `${PUBLISH_DOMAIN}/graphql/execute.json/gs/offerByPath;path=${fragmentPath}`;
 
     const response = await fetch(apiUrl);
     if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
